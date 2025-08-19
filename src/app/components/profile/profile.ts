25ae94b3029a9fa48fd8fe4776bc6d7fecdc9cb4
Pedro Lima <pedrlimadev@gmail.com>
2025-08-19 14:46:51 -0300

    fix: reload router error

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -43,8 +43,9 @@ export class ProfileComponent implements OnInit {
 
   submit(valor: string) {
     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
-      this.router.navigate([`/profile/${valor}`]).then(() => {});
-      window.location.reload();
+      this.router.navigate([`/profile/${valor}`]).then(() => {
+        window.location.reload();
+      });
     });
   }
 }
